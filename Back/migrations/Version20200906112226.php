a6a6cefa3ab001573b6ed29bccbd40524cb16a4a
diikaanedev <diikaanedev@gmail.com>
2020-09-06 11:59:28 +0000

    feature last

--- Back/migrations/Version20200906112226.php
+++ Back/migrations/Version20200906112226.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200905122744 extends AbstractMigration
+final class Version20200906112226 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,7 +20,9 @@ final class Version20200905122744 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE immobilisation ADD inventaitre_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE immobilisation DROP FOREIGN KEY FK_B1563E27CE430A85');
+        $this->addSql('DROP INDEX IDX_B1563E27CE430A85 ON immobilisation');
+        $this->addSql('ALTER TABLE immobilisation ADD inventaitre_id INT DEFAULT NULL, DROP inventaire_id');
         $this->addSql('ALTER TABLE immobilisation ADD CONSTRAINT FK_B1563E27B86294C FOREIGN KEY (inventaitre_id) REFERENCES inventaire (id)');
         $this->addSql('CREATE INDEX IDX_B1563E27B86294C ON immobilisation (inventaitre_id)');
     }
@@ -30,6 +32,8 @@ final class Version20200905122744 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE immobilisation DROP FOREIGN KEY FK_B1563E27B86294C');
         $this->addSql('DROP INDEX IDX_B1563E27B86294C ON immobilisation');
-        $this->addSql('ALTER TABLE immobilisation DROP inventaitre_id');
+        $this->addSql('ALTER TABLE immobilisation ADD inventaire_id INT NOT NULL, DROP inventaitre_id');
+        $this->addSql('ALTER TABLE immobilisation ADD CONSTRAINT FK_B1563E27CE430A85 FOREIGN KEY (inventaire_id) REFERENCES inventaire (id)');
+        $this->addSql('CREATE INDEX IDX_B1563E27CE430A85 ON immobilisation (inventaire_id)');
     }
 }
